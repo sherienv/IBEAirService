e3309a57410e0f3476d499aea03c43a0097e567e
sherin <sherinvarghese09@gmail.com>
2020-06-15 11:43:35 +0530

    first commit

---
+++ src/test/java/com/ibsplc/airshopping/AirshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibsplc.airshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
